a41208706eaa45a1cf8393b6a46d474e0c8ecc24
tanvidabholkar04 <141729517+tanvidabholkar04@users.noreply.github.com>
2023-09-26 15:01:00 +0530

    Update python.py
    update by branch2

--- python.py
+++ python.py
@@ -1,3 +1,4 @@
 #python program will print foss
 print("python is case sensitive language")
-print("python is object oriented language')
+print("python is object oriented language")
+print("python is a programming language")
